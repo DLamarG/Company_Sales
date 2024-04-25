c5419c5f61f28dc8a9f9b1b7bb9c11127b93bb95
DLamarG <demondg8@gmail.com>
2024-04-25 13:05:33 -0500

    final version

--- setup_public.sh
+++ setup_public.sh
@@ -1,2 +1,3 @@
+docker pull dlamarg/sales
 docker build -t dlamarg/sales .
-docker run --rm -e POSTGRES_PASSWORD=password -p 5466:5432 -d dlamarg/sales
+docker run --rm -e POSTGRES_PASSWORD=password -p 5477:5432 -d dlamarg/sales
